349903918f6f7775a46ce46f90d5a4d42e42071d
wangjialin <gloria-wang@163.com>
2020-09-02 15:35:04 +0800

    第二天作业

---
+++ day02/Numpy-05.py
@@ -0,0 +1,3 @@
+import numpy as np
+array = np.arange(6)
+print(np.nonzero(array))
---
+++ day02/Numpy-15.py
@@ -0,0 +1,6 @@
+import numpy as np
+import sys
+np.set_printoptions(threshold=5)
+np.set_printoptions(threshold = sys.maxsize)
+array = np.zeros((15,15))
+print(array)
