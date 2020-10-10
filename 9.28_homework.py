55251cfd3f25ce13f6c0c413ef6e7b4f02b7e300
lcy19991113 <1205467371@qq.com>
2020-10-10 12:36:23 +0800

    9.28

---
+++ 9.28_homework.py
@@ -0,0 +1,405 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+from sklearn.preprocessing import LabelEncoder
+from scipy.stats import norm, skew
+from scipy.special import boxcox1p
+from sklearn.linear_model import ElasticNet, Lasso, BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+
+warnings.filterwarnings('ignore')
+sns.set()
+
+df_train = pd.read_csv('./data/train.csv')
+# print(df_train.columns)
+
+# print(df_train['SalePrice'].describe())
+
+# plt.figure(figsize=(16,8))
+# sns.distplot(df_train['SalePrice'])
+# plt.show()
+
+# skewness and kurtosis 偏度和峰度
+# print("Skewness: %f" % df_train['SalePrice'].skew())
+# print("Kurtosis: %f" % df_train['SalePrice'].kurt())
+
+# 居住面积平方英尺
+# var = 'GrLivArea'
+# data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+# data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000), figsize=(16, 8))
+# plt.show()
+
+# 地下室面积平方英尺
+# var = 'TotalBsmtSF'
+# data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+# data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000), figsize=(16, 8));
+# plt.show()
+
+# var = 'OverallQual'
+# data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+# f, ax = plt.subplots(figsize=(16, 8))
+# fig = sns.boxplot(x=var, y="SalePrice", data=data)
+# fig.axis(ymin=0, ymax=800000);
+# plt.show()
+
+# 原施工日期
+# var = 'YearBuilt'
+# data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+# f, ax = plt.subplots(figsize=(25, 8))
+# fig = sns.boxplot(x=var, y="SalePrice", data=data)
+# fig.axis(ymin=0, ymax=800000);
+# plt.xticks(rotation=90);
+# plt.show()
+
+# var = 'Neighborhood'
+# data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+# f, ax = plt.subplots(figsize=(16, 8))
+# fig = sns.boxplot(x=var, y="SalePrice", data=data)
+# fig.axis(ymin=0, ymax=800000);
+# plt.xticks(rotation=90);
+# plt.show()
+
+# correlation matrix
+# corrmat = df_train.corr()
+# f, ax = plt.subplots(figsize=(30, 30))
+# sns.heatmap(corrmat, square=True,cmap='YlGnBu');
+# plt.show()
+
+# k = 10
+# cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+# cm = np.corrcoef(df_train[cols].values.T)
+# plt.figure(figsize=(16,8))
+# sns.set(font_scale=1.25)
+# hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values,cmap='YlGnBu')
+# plt.show()
+
+# cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+# sns.pairplot(df_train[cols], size = 2.5)
+# plt.show()
+
+# missing data
+# total = df_train.isnull().sum().sort_values(ascending=False)
+# percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+# missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+# print(missing_data.head(20))
+
+################################################################################################
+train = pd.read_csv('./data/train.csv')
+test = pd.read_csv('./data/test.csv')
+
+# 看看数据多大的
+print("The train data size before dropping Id feature is : {} ".format(train.shape))
+print("The test data size before dropping Id feature is : {} ".format(test.shape))
+
+# ID先留着，暂时不用
+train_ID = train['Id']
+test_ID = test['Id']
+
+# 去掉ID
+train.drop("Id", axis=1, inplace=True)
+test.drop("Id", axis=1, inplace=True)
+
+# 看一下处理后的数据的shape
+print("\nThe train data size after dropping Id feature is : {} ".format(train.shape))
+print("The test data size after dropping Id feature is : {} ".format(test.shape))
+
+################
+# fig, ax = plt.subplots(figsize=(16,8))
+# ax.scatter(x = train['GrLivArea'], y = train['SalePrice'])
+# plt.ylabel('SalePrice', fontsize=13)
+# plt.xlabel('GrLivArea', fontsize=13)
+# plt.show()
+#
+# # 干掉离群点
+# train = train.drop(train[(train['GrLivArea']>4000) & (train['SalePrice']<300000)].index)
+#
+# fig, ax = plt.subplots(figsize=(16,8))
+# ax.scatter(train['GrLivArea'], train['SalePrice'])
+# plt.ylabel('SalePrice', fontsize=13)
+# plt.xlabel('GrLivArea', fontsize=13)
+# plt.show()
+################
+
+# plt.figure(figsize=(16,8))
+# sns.distplot(train['SalePrice'] , fit=norm);
+# (mu, sigma) = norm.fit(train['SalePrice'])
+# print( '\n mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+#
+# # 分布图
+# plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)],
+#             loc='best')
+# plt.ylabel('Frequency')
+# plt.title('SalePrice distribution')
+#
+# # QQ图 分位数图示法（Quantile Quantile Plot，简称 QQ图）
+# fig = plt.figure(figsize=(16,8))
+# res = stats.probplot(train['SalePrice'], plot=plt)
+# plt.show()
+#
+# # 对数变换log(1+x)
+# train["SalePrice"] = np.log1p(train["SalePrice"])
+#
+# # 看看新的分布
+# plt.figure(figsize=(16,8))
+# sns.distplot(train['SalePrice'] , fit=norm);
+#
+# # 参数
+# (mu, sigma) = norm.fit(train['SalePrice'])
+# print( '\n mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+#
+# # 画图
+# plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)],
+#             loc='best')
+# plt.ylabel('Frequency')
+# plt.title('SalePrice distribution')
+#
+# # QQ图
+# fig = plt.figure(figsize=(16,8))
+# res = stats.probplot(train['SalePrice'], plot=plt)
+# plt.show()
+###################
+
+# 替换
+ntrain = train.shape[0]
+ntest = test.shape[0]
+y_train = train.SalePrice.values
+all_data = pd.concat((train, test)).reset_index(drop=True)
+all_data.drop(['SalePrice'], axis=1, inplace=True)
+print("all_data size is : {}".format(all_data.shape))
+
+# 缺失值处理
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+missing_data = pd.DataFrame({'Missing Ratio': all_data_na})
+print(missing_data.head(20))
+
+# 绘制直方图
+f, ax = plt.subplots(figsize=(18, 12))
+plt.xticks(rotation='90')
+sns.barplot(x=all_data_na.index, y=all_data_na)
+plt.xlabel('Features', fontsize=15)
+plt.ylabel('Percent of missing values', fontsize=15)
+plt.title('Percent missing data by feature', fontsize=15)
+plt.show()
+
+print(all_data["PoolQC"][:5])
+all_data["PoolQC"] = all_data["PoolQC"].fillna("None")
+print(all_data["PoolQC"][:5])
+
+# 没有特征
+all_data["MiscFeature"] = all_data["MiscFeature"].fillna("None")
+# 通道的入口
+all_data["Alley"] = all_data["Alley"].fillna("None")
+# 栅栏
+all_data["Fence"] = all_data["Fence"].fillna("None")
+# 壁炉
+all_data["FireplaceQu"] = all_data["FireplaceQu"].fillna("None")
+# 到街道的距离，连续值，一般使用中位数
+all_data["LotFrontage"] = all_data.groupby("Neighborhood")["LotFrontage"].transform(
+    lambda x: x.fillna(x.median()))
+# 车库的事，补充上None和0
+for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
+    all_data[col] = all_data[col].fillna('None')
+for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
+    all_data[col] = all_data[col].fillna(0)
+# 地下室的事
+for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
+    all_data[col] = all_data[col].fillna(0)
+for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+    all_data[col] = all_data[col].fillna('None')
+# 砌体
+all_data["MasVnrType"] = all_data["MasVnrType"].fillna("None")
+all_data["MasVnrArea"] = all_data["MasVnrArea"].fillna(0)
+print(all_data['MSZoning'].mode())
+# 一般分区分类，用众数来补充
+all_data['MSZoning'] = all_data['MSZoning'].fillna(all_data['MSZoning'].mode()[0])
+# Functional家庭功能评定
+all_data["Functional"] = all_data["Functional"].fillna("Typ")
+# 电力系统
+all_data['Electrical'] = all_data['Electrical'].fillna(all_data['Electrical'].mode()[0])
+# 厨房的品质
+all_data['KitchenQual'] = all_data['KitchenQual'].fillna(all_data['KitchenQual'].mode()[0])
+# 外部
+all_data['Exterior1st'] = all_data['Exterior1st'].fillna(all_data['Exterior1st'].mode()[0])
+all_data['Exterior2nd'] = all_data['Exterior2nd'].fillna(all_data['Exterior2nd'].mode()[0])
+# 销售类型
+all_data['SaleType'] = all_data['SaleType'].fillna(all_data['SaleType'].mode()[0])
+# 建筑类型
+all_data['MSSubClass'] = all_data['MSSubClass'].fillna("None")
+all_data = all_data.drop(['Utilities'], axis=1)
+
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)
+missing_data = pd.DataFrame({'Missing Ratio': all_data_na})
+print(missing_data.head())
+
+# 有些并不是连续值，给他们做成类别值
+all_data['MSSubClass'] = all_data['MSSubClass'].apply(str)
+
+all_data['OverallCond'] = all_data['OverallCond'].astype(str)
+
+all_data['YrSold'] = all_data['YrSold'].astype(str)
+all_data['MoSold'] = all_data['MoSold'].astype(str)
+
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
+        'ExterQual', 'ExterCond', 'HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1',
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond',
+        'YrSold', 'MoSold')
+# process columns, apply LabelEncoder to categorical features
+for c in cols:
+    lbl = LabelEncoder()
+    lbl.fit(list(all_data[c].values))
+    all_data[c] = lbl.transform(list(all_data[c].values))
+
+# shape
+print('Shape all_data: {}'.format(all_data.shape))
+
+# 增加一个新特征总面积
+all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
+
+numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+
+# 每一列，都算一下偏度
+skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+print("\nSkew in numerical features: \n")
+skewness = pd.DataFrame({'Skew': skewed_feats})
+print(skewness.head(10))
+####################################
+
+
+skewness = skewness[abs(skewness) > 0.75]
+print("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
+
+skewed_features = skewness.index
+lam = 0.15
+for feat in skewed_features:
+    all_data[feat] = boxcox1p(all_data[feat], lam)
+
+all_data = pd.get_dummies(all_data)
+print(all_data.shape)
+
+train = all_data[:ntrain]
+test = all_data[ntrain:]
+
+n_folds = 5
+
+
+def rmsle_cv(model):
+    kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(train.values)
+    rmse = np.sqrt(-cross_val_score(model, train.values, y_train, scoring="neg_mean_squared_error", cv=kf))
+    return (rmse)
+
+
+lasso = make_pipeline(RobustScaler(), Lasso(alpha=0.0005, random_state=1))
+ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+# KRR = KernelRidge(alpha=0.6, kernel='rbf')
+# http://blog.csdn.net/wsj998689aa/article/details/47027365   核函数介绍
+KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10,
+                                   loss='huber', random_state=5)
+model_xgb = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468,
+                             learning_rate=0.05, max_depth=3,
+                             min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571,
+                             subsample=0.5213, silent=1,
+                             nthread=-1)
+score = rmsle_cv(lasso)
+print("\nLasso score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+score = rmsle_cv(ENet)
+print("ElasticNet score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+score = rmsle_cv(KRR)
+print("Kernel Ridge score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+score = rmsle_cv(GBoost)
+print("Gradient Boosting score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+score = rmsle_cv(model_xgb)
+print("Xgboost score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
+
+# 平均模型
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models):
+        self.models = models
+
+    # we define clones of the original models to fit the data in
+    def fit(self, X, y):
+        self.models_ = [clone(x) for x in self.models]
+
+        # Train cloned base models
+        for model in self.models_:
+            model.fit(X, y)
+
+        return self
+
+    # 堆叠预测结果
+    def predict(self, X):
+        predictions = np.column_stack([
+            model.predict(X) for model in self.models_
+        ])
+        return np.mean(predictions, axis=1)
+
+
+averaged_models = AveragingModels(models=(ENet, GBoost, KRR, lasso))
+
+score = rmsle_cv(averaged_models)
+print(" Averaged base models score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
+
+# 用堆叠模型，再算一个结果
+class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5):
+        # 第一阶段
+        self.base_models = base_models
+        # 第二阶段
+        self.meta_model = meta_model
+        self.n_folds = n_folds
+
+    # We again fit the data on clones of the original models
+    def fit(self, X, y):
+        self.base_models_ = [list() for x in self.base_models]
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
+
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        # 把第一阶段的验证集，传到第二阶段作为训练集
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for train_index, holdout_index in kfold.split(X, y):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+                instance.fit(X[train_index], y[train_index])
+                y_pred = instance.predict(X[holdout_index])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+
+        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+
+    # Do the predictions of all base models on the test data and use the averaged predictions as
+    # meta-features for the final prediction which is done by the meta-model
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_])
+        return self.meta_model_.predict(meta_features)
+
+
+stacked_averaged_models = StackingAveragedModels(base_models=(ENet, GBoost, KRR), meta_model=lasso)
+
+score = rmsle_cv(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f} ({:.4f})".format(score.mean(), score.std()))
