bbeb4e05072e5d8aafeab67d42c764c16ca9ef1f
boomcurr28 <32975237+boomcurr28@users.noreply.github.com>
2020-10-17 10:02:05 +0800

    第四次大作业

---
+++ catanddog.py
@@ -0,0 +1,105 @@
+import os
+import warnings
+warnings.filterwarnings("ignore")
+import time
+import datetime
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#训练和验证
+# 数据所在文件夹
+base_dir = './data/cats_and_dogs'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# 训练集
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# 验证集
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+#构建卷积神经网络模型
+model = tf.keras.models.Sequential([
+    # 如果训练慢，可以把数据设置的更小一些
+    # 32：filters；
+    # (3,3)：kernel_size；
+    # MaxPooling2D(2, 2)：W、H为原来的1/2
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(64, 64, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    
+    # 为全连接层准备，Flatten拉长操作
+    tf.keras.layers.Flatten(),
+    
+    tf.keras.layers.Dense(512, activation='relu'),
+    # 二分类sigmoid就够了
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+#配置训练器
+#损失函数，优化器，评估标准
+model.compile(loss='binary_crossentropy',
+              optimizer=Adam(lr=1e-4),
+              metrics=['acc'])
+
+#数据预处理
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # 文件夹路径
+        target_size=(64, 64),  # 指定resize成的大小
+        batch_size=20,
+        # 如果one-hot就是categorical，二分类用binary就可以
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(64, 64),
+        batch_size=20,
+        class_mode='binary')
+
+#训练网络模型
+start_time = time.time()
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,  # 2000 images = batch_size * steps
+      epochs=20,
+      validation_data=validation_generator,
+      validation_steps=50,  # 1000 images = batch_size * steps
+      verbose=2)
+print(datetime.timedelta(seconds = time.time() - start_time))
+
+#效果展示
+plt.style.use('classic')
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
