de4a27457e12e7ef4f501e37391c368d5b315e0b
wuhongxu123 <3307771422@qq.com>
2020-10-25 16:56:14 +0800

    第五次大作业

---
+++ ResNet/models/residual_block.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+
+
+class BasicBlock(tf.keras.layers.Layer):
+
+    def __init__(self, filter_num, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(filters=filter_num,
+                                            kernel_size=(3, 3),
+                                            strides=stride,
+                                            padding="same")
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.conv2 = tf.keras.layers.Conv2D(filters=filter_num,
+                                            kernel_size=(3, 3),
+                                            strides=1,
+                                            padding="same")
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        if stride != 1:
+            self.downsample = tf.keras.Sequential()
+            self.downsample.add(tf.keras.layers.Conv2D(filters=filter_num,
+                                                       kernel_size=(1, 1),
+                                                       strides=stride))
+            self.downsample.add(tf.keras.layers.BatchNormalization())
+        else:
+            self.downsample = lambda x: x
+
+    def call(self, inputs, training=None):
+        identity = self.downsample(inputs)
+
+        conv1 = self.conv1(inputs)
+        bn1 = self.bn1(conv1, training=training)
+        relu = tf.nn.relu(bn1)
+        conv2 = self.conv2(relu)
+        bn2 = self.bn2(conv2, training=training)
+
+        output = tf.nn.relu(tf.keras.layers.add([identity, bn2]))
+
+        return output
+
+
+class BottleNeck(tf.keras.layers.Layer):
+    def __init__(self, filter_num, stride=1, with_downsample=True):
+        super(BottleNeck, self).__init__()
+        self.with_downsample = with_downsample
+        self.conv1 = tf.keras.layers.Conv2D(filters=filter_num,
+                                            kernel_size=(1, 1),
+                                            strides=1,
+                                            padding='same')
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.conv2 = tf.keras.layers.Conv2D(filters=filter_num,
+                                            kernel_size=(3, 3),
+                                            strides=stride,
+                                            padding='same')
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.conv3 = tf.keras.layers.Conv2D(filters=filter_num * 4,
+                                            kernel_size=(1, 1),
+                                            strides=1,
+                                            padding='same')
+        self.bn3 = tf.keras.layers.BatchNormalization()
+
+        self.downsample = tf.keras.Sequential()
+        self.downsample.add(tf.keras.layers.Conv2D(filters=filter_num * 4,
+                                                   kernel_size=(1, 1),
+                                                   strides=stride))
+        self.downsample.add(tf.keras.layers.BatchNormalization())
+
+    def call(self, inputs, training=None):
+        identity = self.downsample(inputs)
+
+        conv1 = self.conv1(inputs)
+        bn1 = self.bn1(conv1, training=training)
+        relu1 = tf.nn.relu(bn1)
+        conv2 = self.conv2(relu1)
+        bn2 = self.bn2(conv2, training=training)
+        relu2 = tf.nn.relu(bn2)
+        conv3 = self.conv3(relu2)
+        bn3 = self.bn3(conv3, training=training)
+        # 判断，是否做1x1的卷积
+        if self.with_downsample == True:
+            output = tf.nn.relu(tf.keras.layers.add([identity, bn3]))
+        else:
+            output = tf.nn.relu(tf.keras.layers.add([inputs, bn3]))
+
+        return output
+
+
+def build_res_block_1(filter_num, blocks, stride=1):
+    res_block = tf.keras.Sequential()
+    res_block.add(BasicBlock(filter_num, stride=stride))
+
+    for _ in range(1, blocks):
+        res_block.add(BasicBlock(filter_num, stride=1))
+
+    return res_block
+
+
+def build_res_block_2(filter_num, blocks, stride=1):
+    res_block = tf.keras.Sequential()
+    res_block.add(BottleNeck(filter_num, stride=stride))
+
+    for _ in range(1, blocks):
+        res_block.add(BottleNeck(filter_num, stride=1, with_downsample=False))
+
+    return res_block
